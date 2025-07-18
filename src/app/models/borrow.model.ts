fd1023cfdb51c58ce30deb4c321eae984d5b22d3
Abdul Muhaimin Toha <toha@notionhive.com>
2025-07-19 01:00:34 +0600

    borrow schema and interface defined

---
+++ src/app/models/borrow.model.ts
@@ -0,0 +1,24 @@
+import { model, Schema } from 'mongoose';
+import { IBorrow } from '../interfaces/borrow.interface';
+
+const borrowSchema = new Schema<IBorrow>(
+   {
+      book: {
+         type: Schema.Types.ObjectId,
+         ref: 'Book',
+         required: [true, 'Book ID is required'],
+      },
+      quantity: {
+         type: Number,
+         required: [true, 'Quantity is required'],
+         min: [1, 'Quantity must be at least 1'],
+      },
+      dueDate: {
+         type: Date,
+         required: [true, 'Due date is required'],
+      },
+   },
+   { timestamps: true, versionKey: false }
+);
+
+export const Borrow = model<IBorrow>('Borrow', borrowSchema);
